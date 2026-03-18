121e179aa343cba977ea204220e53f518b981b11
savithavasanth17102005-art <savithavasanth17102005@gmail.com>
2026-03-18 21:15:53 +0530

    final fix

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-
+const path = require("path");  
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "login.html"));
+});
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
